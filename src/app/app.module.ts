998dea1a8f3c318b21ce36ed22da51baab41fc42
NickZRS <nick1804js@gmail.com>
2023-04-14 15:41:46 -0300

    consumo_01

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CalculoComponent } from './calculo/calculo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CalculoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
