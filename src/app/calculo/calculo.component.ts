98487db96ea514dd49cc3bbaa8753b36bca4c1e9
NickZRS <nick1804js@gmail.com>
2023-04-17 23:59:12 -0300

    consumo_02

--- src/app/calculo/calculo.component.ts
+++ src/app/calculo/calculo.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./calculo.component.css']
 })
 export class CalculoComponent {
+  
+  pessoas: number = 0;
+  gstbanho: number = 0;
+  comodos: number = 0;
+  gstcomodos: number = 0;
+  maqroupa: boolean = false;
+  gstmaq: number = 0;
+  secroupa: boolean = false;
+  gstsec: number = 0;
+  tv:number = 0;
+  gsttv: number = 0;
+  comp:number = 0;
+  gstcomp: number = 0;
+  energia:number = 0;
+  kwh: number =0;
+  resp: number = 0;
+
+  calculo(){
+
+    this.gstbanho = this.pessoas * 22.5;
+    this.gstcomodos = this.comodos * 6.00;
+    this.gsttv = this.tv * 12.45;
+    this.gstcomp = this.comp * 15.12;
+    if(this.maqroupa == true){
+      this.gstmaq =  1.76;
+    }
+    if(this.secroupa == true){
+      this.gstsec = 14.92;
+    }
+
+    this.kwh = this.gstbanho + this.gsttv + this.gstcomp + this.gstmaq + this.gstsec;
+    this.resp = this.kwh * this.energia;
+
+  }
 
 }
